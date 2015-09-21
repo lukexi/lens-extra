9463f3906380ff414c86d4d569b8d5ab2a38881a
Luke Iannini <lukexi@me.com>
2015-09-21 01:59:36 -0700

    Type signatures, export operators correctly

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Strict versions of the lens MonadState operators
+Strict versions of the lens MonadState operators to help avoid thunk leaks
--- src/Control/Lens/Extra.hs
+++ src/Control/Lens/Extra.hs
@@ -1,5 +1,6 @@
 module Control.Lens.Extra 
   ( module Extra
+  , module Control.Lens.Extra
   ) where
 
 import Control.Lens as Extra hiding 
@@ -15,26 +16,38 @@ import Control.Monad.State
 forceState :: (MonadState s m) => m ()
 forceState = do x <- get; x `seq` return ()
 
+-- | (.=) using strict modify'
+(.=) :: MonadState s m => ASetter s s a b -> b -> m ()
 l .= x = modify' (l .~ x)
 {-# INLINE (.=) #-}
 infix 4 .=
 
+-- | (?=) using strict modify'
+(?=) :: MonadState s m => ASetter s s a (Maybe b) -> b -> m ()
 l ?= x = modify' (l ?~ x)
 {-# INLINE (?=) #-}
 infix 4 ?=
 
+-- | (%=) using strict modify'
+(%=) :: (Profunctor p, MonadState s m) => Setting p s s a b -> p a b -> m ()
 l %= f = modify' (l %~ f)
 {-# INLINE (%=) #-}
 infix 4 %=
 
+-- | (+=) using strict modify'
+(+=) :: (Num a, MonadState s m) => ASetter s s a a -> a -> m ()
 l += x = modify' (l +~ x)
 {-# INLINE (+=) #-}
 infix 4 +=
 
+-- | (-=) using strict modify'
+(-=) :: (Num a, MonadState s m) => ASetter s s a a -> a -> m ()
 l -= x = modify' (l -~ x)
 {-# INLINE (-=) #-}
 infix 4 -=
 
+-- | (*=) using strict modify'
+(*=) :: (Num a, MonadState s m) => ASetter s s a a -> a -> m ()
 l *= x = modify' (l *~ x)
 {-# INLINE (*=) #-}
 infix 4 *=
